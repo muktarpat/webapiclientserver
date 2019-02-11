96665ade9646e0c7c061a7e3f4f868bf2698b815
muktarpat <muktarpat@gmail.com>
2019-02-11 17:21:38 +0530

    added
    commit

---
+++ webapiclientserver/Controllers/userclientController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webapiclientserver.Controllers
+{
+    public class userclientController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
